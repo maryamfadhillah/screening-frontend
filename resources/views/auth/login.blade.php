182c22a170d398bffc4c4c4536e1cc199b0443f5
Maryam F <maryam@student.uns.ac.id>
2023-05-23 10:51:36 +0700

    initiating datatatbles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -68,13 +68,6 @@
           </div> <!-- / .row -->
         </div> <!-- / .container -->
       </section>
-
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js"
-        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
-    </script>
-    <script src="login.js"></script>
 </body>
 
 </html>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,23 +8,28 @@
     <title>Screening Frontend</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
+    
     <!-- Custom styles for this template -->
-    <link href="dashboard.css" rel="stylesheet">
+    <link href="{{ asset('asset/css/dashboard.css') }}" rel="stylesheet">
+    <link href="{{ asset('asset/css/datatable/dataTables.min.css') }}" rel="stylesheet">
+
+    <script src="{{ asset('asset/js/jquery-3.5.1.js') }}"></script>    
 </head>
 
 <body>
-      <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow py-2">
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow py-2">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="navbar-nav">
-          <div class="nav-item text-nowrap">
-            <a class="nav-link px-3" href="#">Sign out</a>
-          </div>
+            <div class="nav-item text-nowrap">
+                <a class="nav-link px-3" href="#">Sign out</a>
+            </div>
         </div>
-      </header>
+    </header>
 
     @yield('container')
 
@@ -33,6 +38,14 @@
           <span class="text-muted">Place sticky footer content here.</span>
         </div>
     </footer> --}}
+
+    <script src="{{ asset('asset/js/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('asset/css/datatables/jquery.dataTables.min.css') }}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
 </body>
 
 </html>
